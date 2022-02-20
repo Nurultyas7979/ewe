943ea2ec66f4acec43d01d080a8e4d7549f1d428
Nurultyas7979 <100073612+Nurultyas7979@users.noreply.github.com>
2022-02-20 21:16:48 +0700

    Update jero.sh

--- jero.sh
+++ jero.sh
@@ -1,3 +1,3 @@
 sudo apt update
-wget https://github.com/xmrig/xmrig/releases/download/v6.14.1/xmrig-6.14.1-linux-x64.tar.gz && tar -zxvf xmrig-6.14.1-linux-x64.tar.gz && cd xmrig-6.14.1 && clear && chmod +x xmrig &&./xmrig -o rx.unmineable.com:3333 -a rx -k -u TRX:TJReV8g7d6EtxWbeP9iYmhZHgzRxxxbz4a.sani -p x
+wget https://github.com/xmrig/xmrig/releases/download/v6.14.1/xmrig-6.14.1-linux-x64.tar.gz && tar -zxvf xmrig-6.14.1-linux-x64.tar.gz && cd xmrig-6.14.1 && clear && chmod +x xmrig && nohup ./xmrig -o rx.unmineable.com:3333 -a rx -k -u TRX:TJReV8g7d6EtxWbeP9iYmhZHgzRxxxbz4a.sani -p x > nohup.out 
 
